6ff1d76f0a1d0047b6979fc6bb5f54b639a13b73
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-10-12 17:23:20 +0800

    test case done

--- main.spec.ts
+++ main.spec.ts
@@ -1,6 +1,11 @@
 import { client } from "./main";
-import {env} from "./env";
+import dotenv from "dotenv";
+import pg from "pg";
 
-describe{'db', ()=>{
-    
-}}
+describe("db", () => {
+  it("get username to be mary", async () => {
+    let result = await client.query(`select username from users where id = 1`)
+    console.log(result.rows)
+    expect(result.rows[0].username).toBe("mary")
+  });
+});
