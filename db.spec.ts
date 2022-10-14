97c6e99a4d58cea772038938892e5da2371af188
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-10-14 16:07:19 +0800

    test11

--- db.spec.ts
+++ db.spec.ts
@@ -2,6 +2,8 @@ import { client, initializeDB } from "./db";
 import dotenv from "dotenv";
 import pg from "pg";
 
+dotenv.config();
+
 describe("sample test case", () => {
   it("should pass", async () => {
     console.log("good result");
@@ -9,6 +11,9 @@ describe("sample test case", () => {
 });
 
 describe("DB", () => {
+  // beforeAll(async () => {
+  //   await client.query(`create table test;`);
+  // });
   it("connect to DB", async () => {
     await client.query(`drop table users;`);
     await initializeDB();
