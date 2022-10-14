2cecb2d37a784915cf8b621ed493d7aa652c6c9e
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-10-14 17:01:40 +0800

    test 15

--- db.ts
+++ db.ts
@@ -8,6 +8,7 @@ export const client = new Client({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST
 });
 
 client.connect();
