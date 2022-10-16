590dc5f08dc260ea3c4bbfb286d7920d2356099e
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-10-17 00:09:33 +0800

    build sanic server

---
+++ pythonServer.py
@@ -0,0 +1,11 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic("my-hello-world-sanic")
+
+@app.route("/")
+def test(request):
+    return json({"hello": "world Sanic"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
