b2d1e46b59cbe9086e37ffe76a292f8c4ca3bff1
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-10-17 14:30:18 +0800

    add blob2

--- public/index.js
+++ public/index.js
@@ -24,7 +24,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 navigator.mediaDevices
   .getUserMedia(constraints) 
   .then((stream) => {
-    video.srcObject = stream;
+    video.srcObject = mediaStream;
     video.play();
   })
   .catch((error) => {
@@ -40,14 +40,13 @@ navigator.mediaDevices
   });
 }
 
-document.querySelector('#snapBtn').addEventListener("click",()=>{
-  // event.preventDefault()
-  context.drawImage(video,0,0,640,480) 
-  let image = new Image()
-  canvas.toBlob(function(blob){
-    const formData = new FormData();
-    formData.append('video', blob);
-    console.log(formData);
-    // fetch();
-  },'image/jpg')
-})
+// document.querySelector('#snapBtn').addEventListener("click",()=>{
+//   // event.preventDefault()
+//   context.drawImage(video,0,0,640,480) 
+//   canvas.toBlob(function(blob){
+//     const formData = new FormData();
+//     formData.append('video', blob);
+//     // fetch();
+//   },'image/jpg')
+//   console.log(formData);
+// })
