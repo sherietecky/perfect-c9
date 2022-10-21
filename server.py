52132579cd88ea155fe9dfa5845879f8e4be66fe
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-10-21 17:31:43 +0800

    add quote to yml, python server detect os

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from sanic import Sanic
 from sanic.response import json
 import os
-
-print(os.name)
-
-# import tensorflow as tf
+if os.name == 'posix':
+    import tensorflow as tf
 import numpy as np
 
 app = Sanic("predict")
@@ -22,7 +20,8 @@ def try_request(request):
 
 @app.route("/predict")
 def predict(request):    
-    import tensorflow as tf
+    if os.name != 'posix':
+        import tensorflow as tf
     
     imgSize = 160
     class_names = ['可口可樂', '啤酒', '寶礦力', '橙', '檸檬茶', '牛奶','牛油果', '益力多', '維他奶', '茄子', '蘋果', '西蘭花', '香蕉']
